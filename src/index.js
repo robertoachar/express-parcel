2f8f90eb549e1dfcdabc177dd8825c60f39d444b
Roberto Achar <robertoachar@gmail.com>
2017-12-20 00:08:42 -0200

    feature: add express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports.hello = (done) => {
-  return done(null, 'It works!');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'It works!' });
+});
+
+app.listen(3000, () => {
+  console.log('Express is running...');
+});
