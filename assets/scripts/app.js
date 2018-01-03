61f4c5627d3cf271d5764e89c2f7dc7789f03456
Roberto Achar <robertoachar@gmail.com>
2018-01-03 03:30:19 -0200

    feature: add bootstrap

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,11 @@
-import $ from 'jquery';
+const jquery = require('jquery');
+window.$ = window.jQuery = jquery;
 
-$(document).ready(() => {
+require('../../node_modules/bootstrap/dist/js/bootstrap');
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../css/styles.css';
+
+$('document').ready(() => {
   console.log('It works!');
 });
